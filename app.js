1583eae8254d0b21e878a66db14e35417fc4d3ae
pavelskl <50134119+pavelskl@users.noreply.github.com>
2021-02-04 16:03:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ if (
         info.show();
         info_message
             .text('')
-            .append('<strong>'+payload.notification.title+'</strong>')
-            .append('<em>'+payload.notification.body+'</em>')
+            .append('<strong>'+payload.data.title+'</strong>')
+            .append('<em>'+payload.data.body+'</em>')
         ;
 
         // register fake ServiceWorker for show notification on mobile devices
